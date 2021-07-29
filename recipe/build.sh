ca9d92a2a3c9abb4186a91d73b41cd963e39be70
Matthias Diener <matthias.diener@gmail.com>
2021-07-29 11:35:22 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,6 @@ set -xe
 
 sed -i.bak 's,/usr/bin/perl -w,/usr/bin/env -S perl -w,' colordiff.pl
 
+/usr/bin/env --help
+
 make DESTDIR=$PREFIX INSTALL_DIR=/bin MAN_DIR=/man/man1 ETC_DIR=/etc install
