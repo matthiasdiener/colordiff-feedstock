f3ff68457e1dda0ddd1cc034a65750a664ddcfe3
Matthias Diener <mdiener@illinois.edu>
2021-07-29 11:27:55 -0500

    Use -w

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-sed -i.bak 's,/usr/bin/perl -w,/usr/bin/env perl,' colordiff.pl
+sed -i.bak 's,/usr/bin/perl -w,/usr/bin/env -S perl -w,' colordiff.pl
 
 make INSTALL_DIR= DESTDIR=$PREFIX install
